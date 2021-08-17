ba352702e3e64d904ba73980ec29885b33c691b4
Anton2050 <avvsoft2050@gmail.com>
2021-08-18 00:21:43 +0300

    Fix bugs
    1. fix bugs in bubble sort method.
    2. demonstrate bubble sort method working.

--- src/Homework01.java
+++ src/Homework01.java
@@ -1,18 +1,36 @@
 import java.util.ArrayList;
 
 public class Homework01 {
-    static ArrayList<Integer> bubbleSort(ArrayList<Integer> arrayList){
-        for (int i = arrayList.size() - 2; i > 0; i--){
-            for (int j = 0; j < i; j++){
-                if (arrayList.get(j) > arrayList.get(j + 1)){
+    static ArrayList<Integer> bubbleSort(ArrayList<Integer> arrayList) {
+        for (int i = arrayList.size() - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (arrayList.get(j) > arrayList.get(j + 1)) {
                     int temp = arrayList.get(j + 1);
                     arrayList.set(j + 1, arrayList.get(j));
                     arrayList.set(j, temp);
                 }
             }
         }
-
         return arrayList;
+    }
+
+    static void printArray(ArrayList<Integer> list) {
+        for (int el : list) {
+            System.out.print(el + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i : new int[]{5, -1, -33, 33, 10, 100, 100, 67, 0, 331, -90, 0, 22, 1}) {
+            list.add(i);
+        }
+        System.out.println("Source array: ");
+        printArray(list);
+        Homework01.bubbleSort(list);
+        System.out.println("Sorted array: ");
+        printArray(list);
     }
 
 }
