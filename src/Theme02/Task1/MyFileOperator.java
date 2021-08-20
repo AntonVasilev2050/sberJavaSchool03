663e5034691cc37d2d1031e6a81672054c0aac1d
Anton2050 <avvsoft2050@gmail.com>
2021-08-20 05:02:32 +0300

    Add printSortedByLength

--- src/Theme02/Task1/MyFileOperator.java
+++ src/Theme02/Task1/MyFileOperator.java
@@ -1,12 +1,22 @@
 package Theme02.Task1;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class MyFileOperator {
-    public ArrayList<String> create() {
+
+//    private ArrayList<String> stringArrayList;
+//
+//    public ArrayList<String> getStringArrayList() {
+//        return stringArrayList;
+//    }
+//
+//    public void setStringArrayList(ArrayList<String> stringArrayList) {
+//        this.stringArrayList = stringArrayList;
+//    }
+
+    public static ArrayList<String> create() {
         ArrayList<String> list = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите строку или \"Выход\" чтобы закончить ");
@@ -18,7 +28,7 @@ public class MyFileOperator {
         return list;
     }
 
-    public ArrayList<String> load(String fileName) {
+    public static ArrayList<String> load(String fileName) {
         ArrayList<String> list = new ArrayList<>();
         File file = new File(fileName);
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file)))) {
@@ -28,17 +38,39 @@ public class MyFileOperator {
                 line = reader.readLine();
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("-------!!!!!!------\nОшибка загрузки файла. Введите текст с клавиатуры ");
+            list = create();
         }
         return list;
     }
 
+    public static int wordCount(String fileName){
+        List<String> list = load(fileName);
+        int wordCount = 0;
+        for (String s: list){
+            wordCount += new StringTokenizer(s, " -,.!?:;%&#").countTokens();
+        }
+        return wordCount;
+    }
 
+    public static void printSortedByLength(String fileName){
+        List<String> list = load(fileName);
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String s: list){
+            stringBuilder.append(s).append(" ");
+        }
+        String[] strings = stringBuilder.toString().replaceAll("[-,.!?:;%&#]", " ").split(" ");
+        Arrays.sort(strings, Comparator.comparing(String::length));
+        for (String s: strings){
+            System.out.println(s);
+        }
+    }
 
     public static void main(String[] args) {
-        MyFileOperator myFileOperator = new MyFileOperator();
-        List<String> list = myFileOperator.load("text.txt");
-        list.forEach(System.out::println);
+        MyFileOperator.load("text.txt").forEach(System.out::println);
+        System.out.println("\nОбщее число слов в файле = " + MyFileOperator.wordCount("text.txt"));
+        printSortedByLength("text.txt");
+
     }
 
 }
