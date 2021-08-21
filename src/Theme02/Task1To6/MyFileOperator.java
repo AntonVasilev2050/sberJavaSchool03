d778968464f7dc500c876c38f191fe0e169001a1
Anton2050 <avvsoft2050@gmail.com>
2021-08-21 22:50:36 +0300

    Add ReverseIterator.java

--- src/Theme02/Task1To6/MyFileOperator.java
+++ src/Theme02/Task1To6/MyFileOperator.java
@@ -59,7 +59,7 @@ public class MyFileOperator {
             }
         }
         ArrayList<String> listOfWords = new ArrayList<>(set);
-        Comparator comparator = new StringLengthSort();
+        Comparator<String> comparator = new StringLengthSort();
         listOfWords.sort(comparator);
         listOfWords.forEach(System.out::println);
     }
@@ -77,7 +77,7 @@ public class MyFileOperator {
         }
         TreeSet<String> setOfWords = new TreeSet<>(listOfWords);
         for (String s : setOfWords) {
-            System.out.println(s + ": " + Collections.frequency(listOfWords, s));
+            System.out.println(s + "  -  " + Collections.frequency(listOfWords, s));
         }
     }
 
@@ -89,23 +89,37 @@ public class MyFileOperator {
         }
     }
 
+    public static void printFileReverseOrder(String fileName){
+        List<String> list = load(fileName);
+        List<String> reversedList = new ArrayList<>();
+        ListIterator<String> listIterator = list.listIterator(list.size());
+        while (listIterator.hasPrevious()){
+            reversedList.add(listIterator.previous());
+        }
+        reversedList.forEach(System.out::println);
+    }
+
     public static void printLineByNumber(String fileName){
         List<String> list = load(fileName);
         Scanner scanner = new Scanner(System.in);
         System.out.print("\nВведите номер строки в диапазоне от 1 до " + list.size() + ": " );
-        int stringNumber = scanner.nextInt();
-        while (!(stringNumber < 1) && !(stringNumber > list.size())){
-            System.out.println(list.get(stringNumber - 1));
-            System.out.print("\nВведите еще один номер строки ");
-            stringNumber = scanner.nextInt();
+        try {
+            int stringNumber = scanner.nextInt();
+            while (!(stringNumber < 1) && !(stringNumber > list.size())){
+                System.out.println(list.get(stringNumber - 1));
+                System.out.print("\nВведите еще один номер строки ");
+                stringNumber = scanner.nextInt();
+            }
+            System.out.println("Ошибка ввода!");
+        } catch (Exception e) {
+            System.out.println("Ошибка ввода!");
         }
-        System.out.println("Ошибка ввода!");
     }
 
 
     public static void main(String[] args) {
         String fileName = "text.txt";
-        MyFileOperator.load(fileName).forEach(System.out::println);
+        load(fileName).forEach(System.out::println);
         System.out.println("\nЧисло РАЗЛИЧНЫХ слов в файле = " + MyFileOperator.wordCount(fileName));
 
         System.out.println("\nСписок различных слов файла, отсортированный по возрастанию их длины: ");
@@ -114,8 +128,18 @@ public class MyFileOperator {
         System.out.println("\nЧисло повторений каждого слова в файле: ");
         printWordFrequency(fileName);
 
-        System.out.println("\nСтроки файла в обратном порядке:");
+        System.out.println("\nСтроки файла от конца к началу строки:");
         printReversedStrings(fileName);
+
+        System.out.println("\nСтроки файла в обратном порядке:");
+        printFileReverseOrder(fileName);
+
+        System.out.println("\nСамодельный итератор в обратном порядке:");
+        List<String> list = load(fileName);
+        for (String l: new ReverseIterator<String>(list)){
+            System.out.println(l);
+        }
+
 
         printLineByNumber(fileName);
     }
---
+++ src/Theme02/Task1To6/ReverseIterator.java
@@ -0,0 +1,54 @@
+package Theme02.Task1To6;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class ReverseIterator<T> implements Iterator<String>, Iterable<String> {
+
+    private final List<String> list;
+    private int position;
+
+    public ReverseIterator(List<String> list) {
+        this.list = list;
+        this.position = list.size() - 1;
+    }
+
+    @Override
+    public Iterator<String> iterator() {
+        return this;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return position >= 0;
+    }
+
+    @Override
+    public String next() {
+        return list.get(position--);
+    }
+
+    @Override
+    public void remove() {
+        throw new UnsupportedOperationException();
+    }
+
+//    public static void main(String[] args) {
+//        List<String> list = new ArrayList<String>();
+//        list.add("A");
+//        list.add("B");
+//        list.add("C");
+//        list.add("D");
+//        list.add("E");
+//
+//        for (String s : new ReverseIterator<String>(list)) {
+//            System.out.println(s);
+//        }
+//    }
+}
+
+
+
+
+
