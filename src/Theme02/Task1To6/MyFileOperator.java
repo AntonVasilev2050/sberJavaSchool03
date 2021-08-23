3b6006ef0e42c80220698b725a27ee37b7eb7c32
Anton2050 <avvsoft2050@gmail.com>
2021-08-23 15:39:53 +0300

    Add printFileReverseOrderV2

--- src/Theme02/Task1To6/MyFileOperator.java
+++ src/Theme02/Task1To6/MyFileOperator.java
@@ -32,7 +32,8 @@ public class MyFileOperator {
         }
         return list;
     }
-//  Задание 1: Подсчитайте количество различных слов в файле.
+
+    //  Задание 1: Подсчитайте количество различных слов в файле.
     public static int wordCount(String fileName) {
         List<String> list = load(fileName);
         Set<String> set = new TreeSet<>();
@@ -47,7 +48,7 @@ public class MyFileOperator {
         return set.size();
     }
 
-//  Задание 2: Выведите на экран список различных слов файла, отсортированный по возрастанию их длины
+    //  Задание 2: Выведите на экран список различных слов файла, отсортированный по возрастанию их длины
     public static void printSortedByLength(String fileName) {
         List<String> list = load(fileName);
         Set<String> set = new TreeSet<>();
@@ -65,7 +66,7 @@ public class MyFileOperator {
         listOfWords.forEach(System.out::println);
     }
 
-//  Задание 3: Подсчитайте и выведите на экран сколько раз каждое слово встречается в файле.
+    //  Задание 3: Подсчитайте и выведите на экран сколько раз каждое слово встречается в файле.
     public static void printWordFrequency(String fileName) {
         List<String> list = load(fileName);
         ArrayList<String> listOfWords = new ArrayList<>();
@@ -83,7 +84,7 @@ public class MyFileOperator {
         }
     }
 
-//  Задание 4: Выведите на экран все строки файла в обратном порядке.
+    //  Задание 4: Выведите на экран все строки файла в обратном порядке.
     public static void printReversedStrings(String fileName) {
         List<String> list = load(fileName);
         for (String l : list) {
@@ -92,59 +93,75 @@ public class MyFileOperator {
         }
     }
 
-    public static void printFileReverseOrder(String fileName){
+    //  Вывод строк файла в обратном порядке
+    public static void printFileReverseOrder(String fileName) {
         List<String> list = load(fileName);
         List<String> reversedList = new ArrayList<>();
         ListIterator<String> listIterator = list.listIterator(list.size());
-        while (listIterator.hasPrevious()){
+        while (listIterator.hasPrevious()) {
             reversedList.add(listIterator.previous());
         }
         reversedList.forEach(System.out::println);
     }
 
-//  Задание 6: Выведите на экран строки, номера которых задаются пользователем в произвольном порядке.
-    public static void printLineByNumber(String fileName){
+    //  Вывод строк файла в обратном порядке. Версия 2.
+    public static void printFileReverseOrderV2(String fileName) {
         List<String> list = load(fileName);
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("\nВведите номер строки в диапазоне от 1 до " + list.size() + ": " );
-        try {
-            int stringNumber = scanner.nextInt();
-            while (!(stringNumber < 1) && !(stringNumber > list.size())){
-                System.out.println(list.get(stringNumber - 1));
-                System.out.print("\nВведите еще один номер строки ");
-                stringNumber = scanner.nextInt();
+        Collections.reverse(list);
+        list.forEach(System.out::println);
+    }
+
+//  Задание 5: Реализуйте свой Iterator для обхода списка в обратном порядке.
+        public static void printSelfMadeReverseIterator (String fileName){
+            List<String> list = load(fileName);
+            for (String l : new ReverseIterator<String>(list)) {
+                System.out.println(l);
             }
-            System.out.println("Ошибка ввода!");
-        } catch (Exception e) {
-            System.out.println("Ошибка ввода!");
-        }
-    }
-
-
-    public static void main(String[] args) {
-        String fileName = "text.txt";
-        load(fileName).forEach(System.out::println);
-        System.out.println("\nЧисло РАЗЛИЧНЫХ слов в файле = " + MyFileOperator.wordCount(fileName));
-
-        System.out.println("\nСписок различных слов файла, отсортированный по возрастанию их длины: ");
-        printSortedByLength(fileName);
-
-        System.out.println("\nЧисло повторений каждого слова в файле: ");
-        printWordFrequency(fileName);
-
-        System.out.println("\nСтроки файла от конца к началу строки:");
-        printReversedStrings(fileName);
-
-        System.out.println("\nСтроки файла в обратном порядке:");
-        printFileReverseOrder(fileName);
-
-        System.out.println("\nСамодельный итератор в обратном порядке:");
-        List<String> list = load(fileName);
-        for (String l: new ReverseIterator<String>(list)){
-            System.out.println(l);
         }
 
-        printLineByNumber(fileName);
-    }
+//  Задание 6: Выведите на экран строки, номера которых задаются пользователем в произвольном порядке.
+        public static void printLineByNumber (String fileName){
+            List<String> list = load(fileName);
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("\nВведите номер строки в диапазоне от 1 до " + list.size() + ": ");
+            try {
+                int stringNumber = scanner.nextInt();
+                while (!(stringNumber < 1) && !(stringNumber > list.size())) {
+                    System.out.println(list.get(stringNumber - 1));
+                    System.out.print("\nВведите еще один номер строки ");
+                    stringNumber = scanner.nextInt();
+                }
+                System.out.println("Ошибка ввода!");
+            } catch (Exception e) {
+                System.out.println("Ошибка ввода!");
+            }
+        }
 
-}
+
+        public static void main (String[]args){
+            String fileName = "text.txt";
+            load(fileName).forEach(System.out::println);
+            System.out.println("\nЧисло РАЗЛИЧНЫХ слов в файле = " + MyFileOperator.wordCount(fileName));
+
+            System.out.println("\nСписок различных слов файла, отсортированный по возрастанию их длины: ");
+            printSortedByLength(fileName);
+
+            System.out.println("\nЧисло повторений каждого слова в файле: ");
+            printWordFrequency(fileName);
+
+            System.out.println("\nСтроки файла от конца к началу строки:");
+            printReversedStrings(fileName);
+
+            System.out.println("\nСтроки файла в обратном порядке:");
+            printFileReverseOrder(fileName);
+
+            System.out.println("\nСтроки файла в обратном порядке версия 2:");
+            printFileReverseOrderV2(fileName);
+
+            System.out.println("\nСамодельный итератор в обратном порядке:");
+            printSelfMadeReverseIterator(fileName);
+
+            printLineByNumber(fileName);
+        }
+
+    }
