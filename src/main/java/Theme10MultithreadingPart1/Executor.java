398caa888130f30b365cf01541b35a2622fd8d93
Anton2050 <avvsoft2050@gmail.com>
2021-09-15 00:13:31 +0300

    Add various multithreading solutions

--- src/main/java/Theme10MultithreadingPart1/Executor.java
+++ src/main/java/Theme10MultithreadingPart1/Executor.java
@@ -4,6 +4,7 @@ import java.io.FileInputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.CountDownLatch;
 
 public class Executor {
     public static List<Integer> list = new ArrayList<>();
@@ -18,19 +19,76 @@ public class Executor {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println(list);
         readFromDisk("src/main/java/Theme10MultithreadingPart1/numbers.txt");
         System.out.println(list);
-        for (int i= 0; i < list.size(); i++){
+        List<Thread> threadList = new ArrayList<>();
+
+        long before = System.currentTimeMillis();
+        for (int numberToCalculate : list) {
+            System.out.println("Factorial of " + numberToCalculate + " - " + ExtraMath.factorial(numberToCalculate));
+        }
+        System.out.println("Processing time " + (System.currentTimeMillis() - before));
+
+        int listSize = list.size();
+        long beforeC = System.currentTimeMillis();
+        for (int i = 0; i < listSize; i++) {
+            int finalI = i;
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    int numberToCalculate = list.get(finalI);
+                    System.out.println("Factorial of " + numberToCalculate + " - " + ExtraMath.factorial(numberToCalculate));
+                }
+            });
+            thread.start();
+            threadList.add(thread);
+        }
+        for (Thread thread : threadList) {
+            thread.join();
+        }
+        System.out.println("Processing time with concurrency " + (System.currentTimeMillis() - beforeC));
+
+        long beforeWithLatch = System.currentTimeMillis();
+        final CountDownLatch latch = new CountDownLatch(listSize);
+        for (int i = 0; i < listSize; i++) {
             int finalI = i;
             new Thread(new Runnable() {
                 @Override
                 public void run() {
                     int numberToCalculate = list.get(finalI);
                     System.out.println("Factorial of " + numberToCalculate + " - " + ExtraMath.factorial(numberToCalculate));
+                    latch.countDown();
                 }
             }).start();
         }
+        latch.await();
+        System.out.println("Processing time with concurrency " + (System.currentTimeMillis() - beforeWithLatch));
+
+        long beforeTwo = System.currentTimeMillis();
+        final CountDownLatch latch1 = new CountDownLatch(2);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    int numberToCalculate = list.get(i);
+                    System.out.println("Factorial of " + numberToCalculate + " - " + ExtraMath.factorial(numberToCalculate));
+                }
+                latch1.countDown();
+            }
+        }).start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 10; i < listSize; i++) {
+                    int numberToCalculate = list.get(i);
+                    System.out.println("Factorial of " + numberToCalculate + " - " + ExtraMath.factorial(numberToCalculate));
+                }
+                latch1.countDown();
+            }
+        }).start();
+        latch1.await();
+        System.out.println("Processing time with two threads " + (System.currentTimeMillis() - beforeTwo));
     }
 }
