69549f98b1d80b97e54b53028c58720736cba8b8
Anton2050 <avvsoft2050@gmail.com>
2021-09-22 17:27:27 +0300

    Add Theme11 FixedThreadPool

--- src/main/java/Theme11MultithreadingPart2/FixedThreadPool.java
+++ src/main/java/Theme11MultithreadingPart2/FixedThreadPool.java
@@ -3,6 +3,8 @@ package Theme11MultithreadingPart2;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class FixedThreadPool implements ThreadPool {
     private int numberOfThreads;
@@ -15,18 +17,18 @@ public class FixedThreadPool implements ThreadPool {
 
     @Override
     public void start() {
-        CountDownLatch latch = new CountDownLatch(3);
-        while (!runnableQueue.isEmpty()) {
-            for (int i = 0; i < numberOfThreads; i++) {
-                new Thread(runnableQueue.poll()).start();
-                latch.countDown();
-            }
-            try {
-                latch.await();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+        CountDownLatch latch = new CountDownLatch(numberOfThreads);
+        while (!runnableQueue.isEmpty()){
+            Thread thread = new Thread(runnableQueue.poll());
+            thread.start();
+            latch.countDown();
         }
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
 
     @Override
@@ -38,12 +40,19 @@ public class FixedThreadPool implements ThreadPool {
         Runnable runnable1 = new Runnable() {
             @Override
             public void run() {
+                System.out.println("1-start");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
                 System.out.println("1");
             }
         };
         Runnable runnable2 = new Runnable() {
             @Override
             public void run() {
+                System.out.println("2-start");
                 try {
                     Thread.sleep(2000);
                 } catch (InterruptedException e) {
@@ -55,19 +64,29 @@ public class FixedThreadPool implements ThreadPool {
         Runnable runnable3 = new Runnable() {
             @Override
             public void run() {
+                System.out.println("3-start");
+
                 System.out.println("3");
             }
         };
         Runnable runnable4 = new Runnable() {
             @Override
             public void run() {
+                System.out.println("4-start");
+                try {
+                    Thread.sleep(4000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
                 System.out.println("4");
             }
         };
-        FixedThreadPool pool = new FixedThreadPool(3);
+        FixedThreadPool pool = new FixedThreadPool(4);
         pool.execute(runnable1);
         pool.execute(runnable2);
         pool.execute(runnable3);
+        pool.execute(runnable4);
+        pool.execute(runnable4);
         pool.execute(runnable4);
         pool.start();
     }
