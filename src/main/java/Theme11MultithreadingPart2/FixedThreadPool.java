4ccd1a28d2c7441f836a5b98c6e54fe0ee2ce278
Anton2050 <avvsoft2050@gmail.com>
2021-09-23 15:22:38 +0300

    Add Theme11 FixedThreadPool

--- src/main/java/Theme11MultithreadingPart2/FixedThreadPool.java
+++ src/main/java/Theme11MultithreadingPart2/FixedThreadPool.java
@@ -5,35 +5,49 @@ import java.util.Queue;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class FixedThreadPool implements ThreadPool {
-    private int numberOfThreads;
-    private Queue<Runnable> runnableQueue = new LinkedList<>();
-    private static final Object LOCK = new Object();
+    private final int numberOfThreads;
+    private final LinkedBlockingQueue<Runnable> runnableQueue;
 
     public FixedThreadPool(int numberOfThreads) {
         this.numberOfThreads = numberOfThreads;
+        runnableQueue = new LinkedBlockingQueue();
+
+    }
+
+    private class PoolWorker extends Thread{
+        @Override
+        public void run() {
+            Runnable runnable;
+            while (true){
+                synchronized (runnableQueue){
+                    while (runnableQueue.isEmpty()){
+                        try {
+                            runnableQueue.wait();
+                        } catch (InterruptedException e) {
+                            System.out.println("An error occurred while queue is waiting: " + e.getMessage());
+                        }
+                    }
+                    runnable = runnableQueue.poll();
+                }
+                runnable.run();
+            }
+        }
     }
 
     @Override
-    public void start() {
-        CountDownLatch latch = new CountDownLatch(numberOfThreads);
-        while (!runnableQueue.isEmpty()){
-            Thread thread = new Thread(runnableQueue.poll());
-            thread.start();
-            latch.countDown();
+    public void start()  {
+        for(int i = 0; i < numberOfThreads; i++){
+            new PoolWorker().start();
         }
-        try {
-            latch.await();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
     }
 
     @Override
     public void execute(Runnable runnable) {
         runnableQueue.add(runnable);
+//        runnableQueue.notifyAll();
     }
 
     public static void main(String[] args) {
@@ -54,7 +68,7 @@ public class FixedThreadPool implements ThreadPool {
             public void run() {
                 System.out.println("2-start");
                 try {
-                    Thread.sleep(2000);
+                    Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -65,7 +79,11 @@ public class FixedThreadPool implements ThreadPool {
             @Override
             public void run() {
                 System.out.println("3-start");
-
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
                 System.out.println("3");
             }
         };
@@ -74,20 +92,22 @@ public class FixedThreadPool implements ThreadPool {
             public void run() {
                 System.out.println("4-start");
                 try {
-                    Thread.sleep(4000);
+                    Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
                 System.out.println("4");
             }
         };
-        FixedThreadPool pool = new FixedThreadPool(4);
+        FixedThreadPool pool = new FixedThreadPool(3);
+        pool.start();
         pool.execute(runnable1);
         pool.execute(runnable2);
         pool.execute(runnable3);
         pool.execute(runnable4);
         pool.execute(runnable4);
+        pool.execute(runnable2);
         pool.execute(runnable4);
-        pool.start();
+
     }
 }
