8c621b7768c63bd9fdc472920889bd4d8c698937
Anton2050 <avvsoft2050@gmail.com>
2021-10-14 10:10:46 +0300

    Add start method to FileDownloader

--- src/main/java/theme17spring/FileDownloader.java
+++ src/main/java/theme17spring/FileDownloader.java
@@ -1,6 +1,8 @@
 package theme17spring;
 
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class FileDownloader {
     ReadUrlStrFromFile readUrlStrFromFile = new ReadUrlStrFromFileImpl();
@@ -12,6 +14,10 @@ public class FileDownloader {
     public void start() {
         //Read list of urls from file
         List<String> list = readUrlStrFromFile.read(fileWithUrls);
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        for (String urlStr: list){
+            executorService.submit(new DownloadTask(urlStr, path, 20));
+        }
     }
 
 //    Create empty files in destination folder
